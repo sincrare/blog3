344cfbc10a4622622d36174ac157b3a5c4b2d550
SINCRARE <SINCRARE@KEIGO-MAC.local>
2018-07-01 04:47:31 +0900

    bug fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   has_many :article_images
   accepts_nested_attributes_for :article_images
 
-  scope :order_by_descending, -> { order (entry_at: :desc)}
+  scope :order_by_descending, -> { order(entry_at: :desc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
   resources :articles, only: [:index, :show]
+
+  namespace :admin do
+    resources :articles
+  end
 end
